51e4411efed57ba5138fc14b628a2f3d4fb04a71
natalie-eizips <104609729+natalie-eizips@users.noreply.github.com>
2024-11-28 19:19:02 +0200

    update

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,63 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String str1a = preProcess(str1);
+		String str2a = preProcess(str2);
+		
+		for (int i = 0; i < str1a.length() - 1; i++){
+			if (letterCount(str1a, str1a.charAt(i)) != letterCount(str2a, str1a.charAt(i))){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static int letterCount(String str, char character){
+		int count = 0;
+		char current = ' ';
+		if (current == ' '){
+			return 0;
+		}
+		for (int i = 0; i < str.length() - 1; i++){
+			current = str.charAt(i);
+			if (current == character){
+				count ++;
+			}
+		}
+		return count;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String new_string = str;
+		String lower_case = "";
+		for (int i = 0; i < new_string.length() - 1; i++){
+			char current = new_string.charAt(i);
+			if (current >= 65 && current <= 90){
+				lower_case += (char) (current + 32);
+			}
+			else if (current == 32 || (current >= 97 && current <= 122)){
+				lower_case += current;
+			}
+		}
+		return lower_case;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String new_string = "";
+
+		while (str.length() > 0){
+			int max = str.length() - 1;
+			int min = 0;
+			int new_index = (int) (Math.random() * ((max-min) + 1)) + min;
+			char new_char = str.charAt(new_index);
+			new_string = new_string + new_char;
+			str = str.substring(0, new_index) + str.substring(new_index + 1);
+		}
+		return new_string;
 	}
 }
